bd97a20d85b917559c79d709f5f66a3c1ee3f454
baimeng-fan <mfdfan@ucdavis.edu>
2024-02-03 11:10:06 -0800

    new

--- studio1/script.js
+++ studio1/script.js
@@ -8,30 +8,54 @@
     myForm.addEventListener('submit',function (event){
         event.preventDefault();
         const noun1=document.querySelector('#noun1').value;
-        const nountwo=document.getElementById('noun2').value;
+        const animal=document.getElementById('animal').value;
+        const nouns=document.getElementById('nouns').value;
+        const emotion=document.getElementById('emotion').value;
+        const creatures=document.getElementById('creatures').value;
+        const substance=document.getElementById('substance').value;
         const adj=document.getElementById('adj').value;
-        const verbb=document.getElementById('verb').value;
+        const noun2=document.getElementById('noun2').value;
+        const noun3=document.getElementById('noun3').value;
 
         let myText;
 
         if(noun1 == ''){
-            myText="please give a noun";
+            myText="Input a Noun";
             document.querySelector('#noun1').focus();
         }
-        else if(nountwo == ''){
-            myText="please give a noun";
-            document.querySelector('#noun2').focus();
+        else if(animal == ''){
+            myText="Input an Animal";
+            document.querySelector('#animal').focus();
         }
-        
-        else if(adj == ''){
-            myText="please give a noun";
+        else if(nouns == ''){
+            myText="Input a Plural Noun";
+            document.querySelector('#nouns').focus();
+        }
+        else if(emotion == ''){
+            myText="Input an Emotion";
+            document.querySelector('#emotion').focus();
+        }
+        else if(creatures==''){
+            myText="Input plural mythical creatures";
+            document.querySelector('#creatures').focus();
+        }
+        else if(substance==''){
+            myText="Input a kind of substance";
+            document.querySelector('#substance').focus();
+        }
+        else if(adj==''){
+            myText="Input adjective";
             document.querySelector('#adj').focus();
         }
-
-        else if(verbb == ''){
-            myText="please give a noun";
-            document.querySelector('#verb').focus();
-        }else{
+        else if(noun2==''){
+            myText="Input another noun";
+            document.querySelector('#noun2').focus();
+        }
+        else if(noun3==''){
+            myText="Input another noun";
+            document.querySelector('#noun3').focus();
+        }
+        else{
             myText=`
             Once upon a time in the fantastical realm of Whimsyville, there lived a character named Silly Sally. Silly Sally was known far and wide for her ability to turn the most mundane situations into adventures. One day, she stumbled upon a mysterious portal that transported her to a land filled with ${noun1} of every imaginable shape and color.
             
